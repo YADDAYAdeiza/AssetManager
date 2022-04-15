62c3d1d8e9d69a8b580a639068360fcaaafd0774
Adeiza Yusuf <hiscript@gmail.com>
2022-04-15 09:33:52 +0100

    mongoose change

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ let mongoose = require('mongoose');
 
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser:true }); //play around with this
 
-const db = mongoose.connection();
+const db = mongoose.connection;
 
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to mongoose'));
