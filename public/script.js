c432140cd44b35b4681d4177e6a622fe37d49c0e
Adeiza Yusuf <hiscript@gmail.com>
2022-08-16 17:54:41 +0100

    Go Live Working

--- public/script.js
+++ public/script.js
@@ -2,8 +2,8 @@ const socket = io('/');
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
     secure:true,
-    host:'/assetmanger.herokuapp.com',
-    port:'9000'
+    host:'0.peerjs.com',
+    port:'443'
 })
 
 const myVideo = document.createElement('video');
