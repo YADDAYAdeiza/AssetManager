293bac4708427b03f3bb782de37ac67aa9165b13
Adeiza Yusuf <hiscript@gmail.com>
2022-08-16 12:27:35 +0100

    Go live

--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@ const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
     secure:true,
     host:'/assetmangerer.herokuapp.com',
-    port:'9000'
+    port:'443'
 })
 
 const myVideo = document.createElement('video');
