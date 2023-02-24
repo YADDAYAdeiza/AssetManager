082db75c199d610940ae3fd8fbe439015ffa5129
Adeiza Yusuf <hiscript@gmail.com>
2023-02-24 16:37:25 +0100

    Deploy

--- basicAuth.js
+++ basicAuth.js
@@ -98,7 +98,8 @@ function permitListsLogin(){
         }
         if (req.user.role =='admin'){
             console.log('Admin thing');
-            query = query.where('_id').ne(null); //basically, all
+            query = query.where('_id').in(req.user.profileId)
+            // query = query.where('_id').ne(null); //basically, all
             // query = userModel.find();
             //do nothing.
             // query = userModel.find();
