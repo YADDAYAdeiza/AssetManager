d3d930f0d5af331543a7b4740bebeb2b31a67315
Adeiza Yusuf <hiscript@gmail.com>
2023-02-28 09:08:53 +0100

    Deploy

--- routes/user.js
+++ routes/user.js
@@ -221,8 +221,9 @@ route.get('/showOrNew', permitListsLogin(), hideNavMenu(), (req, res)=>{ //admin
                                                                 // })
                                                             // }
                                 
-                            }catch {
+                            }catch(e) {
                                 console.log('An error occured');
+                                console.log(e.message);
                                 // res.render('user/index.ejs', {msg: `An error occurred getting the list`, searchParams:req.query, msgClass:'error-message'}); //tying the view to the moongoose model
                                 res.render('register.ejs'); //tying the view to the moongoose model
                             }
